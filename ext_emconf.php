8e7ea96389e090db3cdb3d978d842a56e8c1e28e
Georg Ringer <georgringer@users.noreply.github.com>
2020-02-27 10:19:27 +0100

    [TASK] Allow 10 in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'frontend',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.5.99',
         ],
         'conflicts' => [
         ],
