dc6740846d6bb5a29c108f0478f2380f5a1fe3c1
Georg Ringer <georg.ringer@gmail.com>
2024-11-21 11:46:10 +0100

    [TASK] Allow 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'frontend',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-12.9.99',
+            'typo3' => '9.5.0-13.4.99',
         ],
         'conflicts' => [
         ],
